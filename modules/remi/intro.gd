3609e3c30a9984dbe8d14a1a3c00da314f47e2eb
Rémi Monthiller <monthir@git.inpt.fr>
2023-11-28 18:16:46 +0100

    credits

--- modules/remi/intro.gd
+++ modules/remi/intro.gd
@@ -1,4 +1,7 @@
 extends CanvasLayer
 
+func _ready():
+	BackgroundMusic.play_menu.call_deferred()
+
 func _on_animation_player_global_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://modules/corentin/menu.tscn")
